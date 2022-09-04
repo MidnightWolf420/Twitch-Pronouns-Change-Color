8c9a760a4857de14fe773323e3858b2db08cc4ea
MidnightWolf420 <92683505+MidnightWolf420@users.noreply.github.com>
2022-09-04 09:37:13 -0400

    Update Twitch Pronouns Color.user.js

--- Twitch Pronouns Color.user.js
+++ Twitch Pronouns Color.user.js
@@ -28,12 +28,7 @@
     }
 
     function setCookie(name,value,days) {
-        var expires = "";
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days*24*60*60*1000));
-            expires = "; expires=" + date.toUTCString();
-        }
+        var expires = "; expires=" + new Date(2147483647 * 1000).toUTCString();
         document.cookie = name + "=" + (value || "")  + expires + "; path=/";
     }
     var keyPressed = {}; // You could also use an array
