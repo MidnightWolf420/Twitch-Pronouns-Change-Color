1cfa0bacd1ba07c4d63aa372c9b71ec5bbba26d9
MidnightWolf420 <92683505+MidnightWolf420@users.noreply.github.com>
2024-02-03 12:33:34 -0500

    Update Twitch Pronouns Color.user.js

--- Twitch Pronouns Color.user.js
+++ Twitch Pronouns Color.user.js
@@ -6,6 +6,7 @@
 // @author       You
 // @match        https://twitch.tv/*
 // @match        https://dashboard.twitch.tv/*
+// @match        https://*.twitch.tv/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
 // @grant        none
 // ==/UserScript==
@@ -13,55 +14,54 @@
 (function() {
     'use strict';
     function getLocalStorage(name) {
-        return window.localStorage[name]?window.localStorage[name]:"";
+        return window.localStorage[name] ? window.localStorage[name] : "";
     }
     var keyPressed = {}; // You could also use an array
-    onkeydown = onkeyup = function(e){
+    onkeydown = onkeyup = function(e) {
         e = e || event; // to deal with IE
         var keyDown = (e.type == 'keydown');
         keyPressed[e.keyCode] = keyDown;
         this.setTimeout(() => {
             keyPressed[e.keyCode] = false;
         }, 1500)
-        if(keyPressed[18] && keyPressed[82]) {
+        if (keyPressed[18] && keyPressed[82]) {
             keyPressed[18] = false;
             keyPressed[82] = false;
-            var backgroundColor = window.prompt("Set Pronouns Background (CSS Color)", getLocalStorage("pronoun_background")==""?"green":getLocalStorage("pronoun_background"));
-             localStorage.setItem("pronoun_background", backgroundColor);
-            var textColor = window.prompt("Set Pronouns Text Color (CSS Color)", getLocalStorage("pronoun_text_color")==""?"var(--color-text-base)":getLocalStorage("pronoun_text_color"));
-             localStorage.setItem("pronoun_text_color", textColor);
+            var backgroundColor = window.prompt("Set Pronouns Background (CSS Color)", getLocalStorage("pronoun_background") == "" ? "green" : getLocalStorage("pronoun_background"));
+            localStorage.setItem("pronoun_background", backgroundColor);
+            var textColor = window.prompt("Set Pronouns Text Color (CSS Color)", getLocalStorage("pronoun_text_color") == "" ? "var(--color-text-base)" : getLocalStorage("pronoun_text_color"));
+            localStorage.setItem("pronoun_text_color", textColor);
         }
     }
-    window.addEventListener("load", function(event)
-    {
-        if(getLocalStorage("pronoun_background") == "") {
-            var backgroundColor = window.prompt("Set Pronouns Background (CSS Color)","green");
-             localStorage.setItem("pronoun_background", backgroundColor);
+    window.onload = function() {
+        if (getLocalStorage("pronoun_background") == "") {
+            var backgroundColor = window.prompt("Set Pronouns Background (CSS Color)", "green");
+            localStorage.setItem("pronoun_background", backgroundColor);
         }
-        if(getLocalStorage("pronoun_text_color") == "") {
-            var textColor = window.prompt("Set Pronouns Text Color (CSS Color)","var(--color-text-base)");
-             localStorage.setItem("pronoun_text_color", textColor);
+        if (getLocalStorage("pronoun_text_color") == "") {
+            var textColor = window.prompt("Set Pronouns Text Color (CSS Color)", "var(--color-text-base)");
+            localStorage.setItem("pronoun_text_color", textColor);
         }
         setInterval(() => {
             var styles = document.getElementsByTagName("style")
-            for(var i = 0; i < styles.length; i++) {
+            for (var i = 0; i < styles.length; i++) {
                 var style = styles[i]
-                if(style.outerHTML.includes(`.user-pronoun {`)) {
-                    var user_pronounCss = style.outerHTML.match(/.user-pronoun {(\s|\n|.)+?\}/)[0]
+                if (/\.user-pronoun ?{/.test(style.outerHTML)) {
+                    var user_pronounCss = style.outerHTML.match(/.user-pronoun ?{(\s|\n|.)+?\}/)[0]
                     var textColorRegex = new RegExp(getLocalStorage("pronoun_text_color"), "i")
-                    var startOfStyle = style.outerHTML.match(/.user-pronoun {(\s| |\n){0,}padding: .+?;(\s| |\n){0,}font-weight: .+?;(\s| |\n){0,}border-radius: .+?;(\s| |\n){0,}color: .+?;/)[0];
-                    if(!textColorRegex.test(startOfStyle)) {
+                    var startOfStyle = style.outerHTML.match(/.user-pronoun ?{(\s| |\n){0,}padding: ?.+?;(\s| |\n){0,}font-weight: ?.+?;(\s| |\n){0,}border-radius: ?.+?;(\s| |\n){0,}color: ?.+?;/)[0];
+                    if (!textColorRegex.test(startOfStyle)) {
                         style.outerHTML = style.outerHTML.replace(startOfStyle, startOfStyle.replace(/color:.+?;/, `color: ${getLocalStorage("pronoun_text_color")};`));
                     }
-                    if(!user_pronounCss.includes(`background:`)) {
+                    if (!user_pronounCss.includes(`background:`)) {
                         style.outerHTML = style.outerHTML.replace(startOfStyle, `${startOfStyle}\n  background: ${getLocalStorage("pronoun_background")};`);
                     } else {
-                        if(user_pronounCss != user_pronounCss.replace(/background: .+?;/, `background: ${getLocalStorage("pronoun_background")};`)) {
-                            style.outerHTML = style.outerHTML.replace(user_pronounCss, user_pronounCss.replace(/background: .+?;/, `background: ${getLocalStorage("pronoun_background")};`));
+                        if (user_pronounCss != user_pronounCss.replace(/background: ?.+?;/, `background: ${getLocalStorage("pronoun_background")};`)) {
+                            style.outerHTML = style.outerHTML.replace(user_pronounCss, user_pronounCss.replace(/background: ?.+?;/, `background: ${getLocalStorage("pronoun_background")};`));
                         }
                     }
                 }
             }
-        }, 500)
-    });
+        }, 1000)
+    }();
 })();
